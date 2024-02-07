a8b11acc87cd2e67e8fe89855a6e06b2ad16e896
4geru <westhouse51@gmail.com>
2024-02-08 08:29:56 +0900

    remove global header

--- supabase/functions/line-bot-template/supabaseClient.ts
+++ supabase/functions/line-bot-template/supabaseClient.ts
@@ -5,7 +5,4 @@ export const supabaseClient = (req) => createClient(
     Deno.env.get('SUPABASE_URL') ?? '',
     // Supabase API ANON KEY - env var exported by default.
     Deno.env.get('SUPABASE_ANON_KEY') ?? '',
-    // Create client with Auth context of the user that called the function.
-    // This way your row-level-security (RLS) policies are applied.
-    { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
 )
