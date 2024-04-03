12b49eb5c9004fc6d9d3ec623ad592f12abbdf83
4geru <westhouse51@gmail.com>
2024-04-03 22:03:37 +0900

    refactor

--- supabase/functions/line-bot-template/supabaseClient.ts
+++ supabase/functions/line-bot-template/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-export const supabaseClient = (req) => createClient(
+export const supabaseClient = () => createClient(
     // Supabase API URL - env var exported by default.
     Deno.env.get('SUPABASE_URL') ?? '',
     // Supabase API ANON KEY - env var exported by default.
