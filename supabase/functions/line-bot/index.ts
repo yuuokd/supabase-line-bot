47269cd1aa64af6610de5547621602fcfb75d9a6
yuuokd <y.okada.mt133@gmail.com>
2025-11-24 21:29:04 +0900

    modified

--- supabase/functions/line-bot/index.ts
+++ supabase/functions/line-bot/index.ts
@@ -3,43 +3,98 @@
 // This enables autocomplete, go to definition, etc.
 
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
-import { replyMessage } from './messages.ts'
+import { supabaseClient } from './supabaseClient.ts'
+import { Quiz } from "./quiz.ts"
+import { flashCardMessage, replyMessage } from './messages.ts'
+import { shuffle } from "./lib.ts"
 
 console.log("Hello from Functions!")
 
 serve(async (req) => {
+
   const { events } = await req.json()
   console.log(events)
+
   if (events && events[0]?.type === "message") {
     // 文字列化したメッセージデータ
     let messages:any = [
       {
         "type": "text",
-        "quoteToken": events[0].message.quoteToken
-        "text": events[0].message.text
+        "text": "こんにちは！"
       },
-      "quickReply": {
-          "items": [
-            {
-              "type": "action",
-              "action": {
-                "type": "message",
-                "label": "スタート",
-                "text": "スタート"
-              }
-            },
-            {
-              "type": "action",
-              "action": {
-                "type": "location",
-                "label": "位置を送る"
-              }
-            }
-          ]
+      {
+        "type": "text",
+        "text": "テスト / test で単語を登録できます"
       }
     ]
+
+    if (events[0].message.text === 'スタート') {
+      const { data, error } = await supabaseClient().from('quiz').select('id,question,answer')
+      const quizList = shuffle(data).slice(0, 5)
+      // クイズを開始する
+      messages = [
+        {
+          "type": "text",
+          "text": "問題を始めるよ！"
+        },
+        flashCardMessage(quizList[0].question, {list: quizList})
+      ]
+      console.log({ messages, quizList })
+
+    } else if (events[0].message.text.match(/\//g)) {
+      // MEMO:
+      // 送られたメッセージの中に `/` が含まれている場合は文字列を分割して保存する
+      const [question, answer] = events[0].message.text.split('/')
+      const quiz = new Quiz({question, answer})
+      await quiz.saveToSupabase(supabaseClient())
+      messages = quiz.savedMessages()
+    }
+
     replyMessage(events, messages)
-   }
+  }
+
+  if (events && events[0]?.type === "postback") {
+    const postbackMessages = [
+      {
+        "type": "text",
+        "text": `data：${events[0].postback.data}`
+      }
+    ]
+    const messages = [
+      ...postbackMessages
+    ]
+    
+    const postbackData = JSON.parse(events[0].postback.data)
+    let [first, ...list] = postbackData.list
+
+    if(postbackData.action === 'nextCard') {
+      messages.push({
+          "type": "text",
+          "text": `こたえは「${first.answer}」です`
+      })
+    }
+    
+    if(postbackData.action === 'deleteCard') {
+      await supabaseClient().from('quiz').delete().eq('id', first.id)
+      messages.push({
+          "type": "text",
+          "text": "削除しました"
+      })
+    }
+    
+    if(list.length > 0) {
+       messages.push(
+         flashCardMessage(list[0].question, {list: list}) // 続きの問題を返す
+       )
+     } else {
+       messages.push({
+         "type": "text",
+         "text": `おわったよ！`
+       })
+     }
+
+    replyMessage(events, messages)
+  }
 
   return new Response(
     JSON.stringify({status: 'ok'}),
--- supabase/functions/line-bot/messages.ts
+++ supabase/functions/line-bot/messages.ts
@@ -1,33 +1,97 @@
+class ActionValidator {
+    validate(action) {
+        switch (action.type) {
+            case "postback":
+                this.validatePostback(action)
+                break
+            default:
+                break
+        }
+    }
+
+    validatePostback(action) {
+        if (action.data && action.data.length > 300) {
+            console.error({
+                reason: "postback.data は最大で 300 文字までです",
+                data: action.data,
+            })
+        }
+
+        if (action.label && action.label.length > 20) {
+            console.error({
+                reason: "postback.label は最大で 20 文字までです",
+                data: action.label,
+            })
+        }
+    }
+}
+
 export const flashCardMessage = (question, data?) => {
+    const validator = new ActionValidator()
+
     const actions = [
-      {
-        "type": "postback",
-        "label": "答えを見る",
-        "inputOption": "openRichMenu",
-        "data": JSON.stringify({action: 'nextCard', ...data}),
-      }
+        {
+            type: "postback",
+            label: "答えを見る",
+            inputOption: "openRichMenu",
+            data: JSON.stringify({ action: "nextCard", ...data }),
+        },
+        {
+            type: "postback",
+            label: "削除する",
+            data: JSON.stringify({ action: "deleteCard", ...data }),
+        },
+        {
+            type: "clipboard",
+            label: "コピーする",
+            clipboardText: question,
+        },
     ]
 
-    // valdiation
-    // https://developers.line.biz/en/reference/messaging-api/#action-objects
-    actions.forEach((action) => {
-      if(action.type === 'postback' && action.data.length > 300) {
-        console.error({reason: 'postback.data は最大で 300 文字までです', data: action.data})
-      }
-      if(action.type === 'postback' && action.label.length > 20) {
-        console.error({reason: 'postback.label は最大で 20 文字までです', data: action.label})
-      }
-    })
+    // validation 実行（postback だけが対象）
+    actions.forEach((action) => validator.validate(action))
 
-    return {
-      "type": "template",
-      "altText": "単語帳のメッセージを表示中",
-      "template": {
-        "type": "buttons",
-        "text": `「${question}」`,
-        "actions": actions
-      }
+    const bubble = {
+        type: "bubble",
+        body: {
+            type: "box",
+            layout: "vertical",
+            spacing: "sm",
+            contents: [
+                {
+                    type: "text",
+                    text: question,
+                    wrap: true,
+                    weight: "bold",
+                    size: "xl",
+                },
+            ],
+        },
+        footer: {
+            type: "box",
+            layout: "vertical",
+            spacing: "sm",
+            contents: [
+                {
+                    type: "button",
+                    style: "primary",
+                    action: actions[0], // 答えを見る
+                },
+                {
+                    type: "button",
+                    style: "primary",
+                    color: "#E50000",
+                    action: actions[1], // 削除する
+                },
+                {
+                    type: "button",
+                    action: actions[2], // コピーする
+                },
+            ],
+        },
     }
+
+    return flexMessage(bubble, "単語帳のメッセージを表示中")
 }
 
 export const replyMessage = (events, messages) => {
@@ -54,64 +118,75 @@ export const replyMessage = (events, messages) => {
 }
 
 export const flashCardFlexMessage = (question, data?) => {
-    const bubble = {
-        "type": "bubble",
-        "body": {
-            "type": "box",
-            "layout": "vertical",
-            "spacing": "sm",
-            "contents": [
-                {
-                    "type": "text",
-                    "text": question,
-                    "wrap": true,
-                    "weight": "bold",
-                    "size": "xl"
-                }
-            ]
+    const validator = new ActionValidator()
+
+    const actions = [
+        {
+            type: "postback",
+            label: "答えを見る",
+            data: JSON.stringify({ action: "nextCard", ...data }),
         },
-        "footer": {
-            "type": "box",
-            "layout": "vertical",
-            "spacing": "sm",
-            "contents": [
+        {
+            type: "postback",
+            label: "削除する",
+            data: JSON.stringify({ action: "deleteCard", ...data }),
+        },
+        {
+            type: "clipboard",
+            label: "コピーする",
+            clipboardText: question,
+        },
+    ]
+
+    actions.forEach((action) => validator.validate(action))
+
+    const bubble = {
+        type: "bubble",
+        body: {
+            type: "box",
+            layout: "vertical",
+            spacing: "sm",
+            contents: [
                 {
-                    "type": "button",
-                    "style": "primary",
-                    "action": {
-                        "type": "postback",
-                        "label": "答えを見る",
-                        "data": JSON.stringify({action: 'nextCard', ...data}),
-                    }
+                    type: "text",
+                    text: question,
+                    wrap: true,
+                    weight: "bold",
+                    size: "xl",
+                },
+            ],
+        },
+        footer: {
+            type: "box",
+            layout: "vertical",
+            spacing: "sm",
+            contents: [
+                {
+                    type: "button",
+                    style: "primary",
+                    action: actions[0],
                 },
                 {
-                    "type": "button",
-                    "action": {
-                        "type": "postback",
-                        "label": "削除する",
-                        "data": JSON.stringify({action: 'deleteCard', ...data}),
-                    },
-                    "color": "#E50000",
-                    "style": "primary"
+                    type: "button",
+                    action: actions[1],
+                    color: "#E50000",
+                    style: "primary",
                 },
                 {
-                    "type": "button",
-                    "action": {
-                        "type": "clipboard",
-                        "label": "コピーする",
-                        "clipboardText": question
-                    }
-                }
-            ]
-        }
+                    type: "button",
+                    action: actions[2],
+                },
+            ],
+        },
     }
-    return flexMessage(bubble, '単語帳のメッセージを表示中')
+
+    return flexMessage(bubble, "単語帳のメッセージを表示中")
 }
 
-export const flexMessage = (contents) => {
+export const flexMessage = (contents, altText = "This is a Flex Message") => {
     return {
-        "type": "flex",
-        "altText": "This is a Flex Message",
-        "contents": contents
+        type: "flex",
+        altText,
+        contents,
     }
 }
